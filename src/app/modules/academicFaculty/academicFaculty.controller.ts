9d650807fe5458f4ff9f3609d0b5e36d9490988c
theCodelogy <thecodelogy.dev@gmail.com>
2024-06-10 17:47:41 +0600

    academic faculty and academicDepartment add

---
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -0,0 +1,63 @@
+import httpStatus from 'http-status';
+import { AcademicFacultyServices } from './academicFaculty.service';
+import catchAsyng from '../../utils/catchAsyng';
+import sendResponse from '../../utils/sendResponse';
+
+const createAcademicFaculty = catchAsyng(async (req, res) => {
+  const result = await AcademicFacultyServices.createAcademicFacultyIntoDB(
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is created succesfully',
+    data: result,
+  });
+});
+
+const getAllAcademicFaculties = catchAsyng(async (req, res) => {
+  const result = await AcademicFacultyServices.getAllAcademicFacultiesFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculties are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicFaculty = catchAsyng(async (req, res) => {
+  const { facultyId } = req.params;
+  const result =
+    await AcademicFacultyServices.getSingleAcademicFacultyFromDB(facultyId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is retrieved succesfully',
+    data: result,
+  });
+});
+
+const updateAcademicFaculty = catchAsyng(async (req, res) => {
+  const { facultyId } = req.params;
+  const result = await AcademicFacultyServices.updateAcademicFacultyIntoDB(
+    facultyId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic faculty is updated succesfully',
+    data: result,
+  });
+});
+
+export const AcademicFacultyControllers = {
+  createAcademicFaculty,
+  getAllAcademicFaculties,
+  getSingleAcademicFaculty,
+  updateAcademicFaculty,
+};
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,18 +1,49 @@
+// year semesterCode 4digit number
+
 import { TAcademicSemester } from '../academicSemister/academicSemester.interface';
 import { User } from './user.model';
 
-// find last user id
 const findLastStudentId = async () => {
-  const lastId = await User.findOne(
-    { role: 'student' },
-    { id: 1, _id: 0 },
-  ).sort({createdAt:-1}).lean();
-  return lastId?.id ? lastId.id.substring(6) : undefined;
+  const lastStudent = await User.findOne(
+    {
+      role: 'student',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  //2030 01 0001
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
-// year semester 4digit number
+
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId =await findLastStudentId() || (0).toString();
+  // first time 0000
+  //0001  => 1
+  let currentId = (0).toString(); // 0000 by deafult
+
+  const lastStudentId = await findLastStudentId();
+  // 2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01;
+  const lastStudentYear = lastStudentId?.substring(0, 4); // 2030
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6); // 00001
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
   incrementId = `${payload.year}${payload.code}${incrementId}`;
 
   return incrementId;
